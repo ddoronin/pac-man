b71cc39a42168566ee6b890476e872250229f436
Dima Doronin <doronindm@gmail.com>
2019-04-21 18:39:56 -0400

    add header

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -6,6 +6,9 @@ import styles from './styles.module.scss';
 
 export default () => (
     <article className={styles.app}>
+        <header>
+            <h1>PacMan - The Request Composer</h1>
+        </header>
         <section className={styles.layout}>
             <div className={styles.history}>
                 <History />
